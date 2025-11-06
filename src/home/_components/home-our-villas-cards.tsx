08835849f15ecc0f900f2682b205dee76a989180
Apollo <apollomacabodbod8@gmail.com>
2025-11-06 17:43:47 +0800

    added smooth animation

--- src/home/_components/home-our-villas-cards.tsx
+++ src/home/_components/home-our-villas-cards.tsx
@@ -3,6 +3,7 @@ import modernVilla from "src/assets/villas/modern-villa.jpg";
 import modernVillaWithPool from "src/assets/villas/modern-villa-with-pool.jpg";
 import kuta from "src/assets/villas/kuta.svg";
 import location from "src/assets/villas/location.svg";
+import { motion } from "framer-motion";
 
 export default function HomeOurVillasCards() {
   const data = [
@@ -35,11 +36,35 @@ export default function HomeOurVillasCards() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 max-w-[1200px] lg:mx-auto  px-[1.25em] gap-[1.875em] mt-[4em] mb-[6.25em]">
         {data.map((item) => (
           <div key={item.id} className="flex flex-col gap-[1em]">
-            <div
+            <motion.div
               className="relative  h-[25em] bg-cover bg-center "
               style={{ backgroundImage: `url(${item.image})` }}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                },
+              }}
             >
-              <div className="absolute top-[22.0625em] left-[1em]">
+              <motion.div
+                className="absolute top-[22.0625em] left-[1em]"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, y: 10 },
+                  visible: {
+                    opacity: 1,
+                    y: 0,
+                    transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                  },
+                }}
+              >
                 <div className="flex flex-col py-[0.5em] px-[1em] bg-[#FFF] rounded-[3.125em]">
                   <div className="flex items-center justify-between gap-[0.5em]">
                     <img src={kuta} alt="" />
@@ -49,23 +74,62 @@ export default function HomeOurVillasCards() {
                     </p>
                   </div>
                 </div>
-              </div>
-            </div>
+              </motion.div>
+            </motion.div>
 
-            <p className="text-[#222] font-PlayfairDisplay text-[1.25rem] font-bold ">
+            <motion.p
+              className="text-[#222] font-PlayfairDisplay text-[1.25rem] font-bold "
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                },
+              }}
+            >
               {item.Title}
-            </p>
+            </motion.p>
 
-            <div className="flex items-center gap-[0.5em] ">
+            <motion.div
+              className="flex items-center gap-[0.5em] "
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                },
+              }}
+            >
               <img src={location} alt="" />
               <p className="text-[#828282] font-Inter text-[0.75rem] font-bold uppercase ">
                 {item.location}
               </p>
-            </div>
+            </motion.div>
 
-            <p className="text-[#F08420] font-Inter text-[1.25rem] font-bold uppercase">
+            <motion.p
+              className="text-[#F08420] font-Inter text-[1.25rem] font-bold uppercase"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                },
+              }}
+            >
               {item.price}
-            </p>
+            </motion.p>
           </div>
         ))}
       </div>
