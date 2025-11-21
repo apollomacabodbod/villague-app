25e7288c9e4ef35864f8ec4b191862c8ea4493ca
Apollo <apollomacabodbod8@gmail.com>
2025-11-21 09:27:08 +0800

    added contact hero section

---
+++ src/contact/_components/contact-hero-section.tsx
@@ -0,0 +1,88 @@
+import { Link } from "react-router-dom";
+import home from "src/assets/about-us/home.svg";
+import route from "src/assets/about-us/route.svg";
+import { motion } from "framer-motion";
+
+export default function ContactHeroSection() {
+  return (
+    <>
+      <div className="flex flex-col mt-[11.96875em] mb-[11.0625em] max-w-[1200px] lg:mx-auto  px-[1.25em] gap-[2em]">
+        <motion.p
+          className="text-[#F6D167] font-Inter text-[0.875rem] font-bold uppercase"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
+          contact us
+        </motion.p>
+
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <motion.p
+            className="text-[#FFF] font-PlayfairDisplay text-[4.1rem] lg:text-[4.5rem] font-bold capitalize"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 10 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+              },
+            }}
+          >
+            contact us
+          </motion.p>
+
+          <motion.div
+            className="flex items-center gap-[1em] mt-[1em] sm:mt-[0em]"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 10 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+              },
+            }}
+          >
+            <img
+              src={home}
+              alt=""
+              className="object-cover object-center w-[1em]"
+            />
+            <Link
+              to="/"
+              className="text-[#FFF] font-Inter text-[0.875rem] font-bold uppercase"
+            >
+              home
+            </Link>
+
+            <img
+              src={route}
+              alt=""
+              className="object-cover object-center w-[0.5em]"
+            />
+
+            <Link
+              to="/pricing"
+              className="text-[#FFF] font-Inter text-[0.875rem] font-bold uppercase"
+            >
+              contact us
+            </Link>
+          </motion.div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/contact/page.tsx
+++ src/contact/page.tsx
@@ -1,25 +1,19 @@
-import { Button } from "src/components/button";
-import { motion } from "framer-motion";
+import modernMansion from "src/assets/contact/modern-mansion.jpg";
+import ContactHeroSection from "./_components/contact-hero-section";
 
 export default function Contact() {
   return (
     <>
-      <div className="flex flex-col px-[2em] bg-orange-400">
-        <p className="border border-[#50B498] green">test</p>
-        <Button>Destructive</Button>
-        <motion.p
-          className="font-roboto"
-          initial={{ opacity: 0, y: 100 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true, amount: 0.5 }}
-        >
-          demo
-        </motion.p>
-        <p className="">demo2</p>
-      </div>
+      <div
+        className="relative flex flex-col bg-cover bg-center"
+        style={{ backgroundImage: `url(${modernMansion})` }}
+      >
+        <div className="absolute inset-0 bg-[rgba(34,34,34,0.50)]"></div>
 
-      <div className="flex flex-col min-h-screen bg-red-400"></div>
+        <div className="z-10">
+          <ContactHeroSection />
+        </div>
+      </div>
     </>
   );
 }
