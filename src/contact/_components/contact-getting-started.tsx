f67fcb32c5584174aae23d3e4550755eb82f3a0a
Apollo <apollomacabodbod8@gmail.com>
2025-11-21 10:25:47 +0800

    added iframe title

--- src/contact/_components/contact-getting-started.tsx
+++ src/contact/_components/contact-getting-started.tsx
@@ -93,6 +93,7 @@ export default function ContactGettingStarted() {
             </div>
 
             <iframe
+              title="Demografie Karte Deutschland"
               width="100%"
               height="300"
               src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=021%20Hollywood%20Boulevard,%20LA+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
