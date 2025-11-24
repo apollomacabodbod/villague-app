018e5e1c8da5108a61bfae0fbad7edb0c59db56a
Apollo <apollomacabodbod8@gmail.com>
2025-11-24 10:08:04 +0800

    added latest blog posts

---
+++ src/blog/_components/blog-latest-posts.tsx
@@ -0,0 +1,44 @@
+import { motion } from "framer-motion";
+
+export default function BlogLatestPosts() {
+  return (
+    <>
+      <div className="flex flex-col mt-[6.25em] gap-[2em] max-w-[1200px] lg:mx-auto  px-[1.25em]">
+        <motion.p
+          className="text-[#222] font-PlayfairDisplay text-[2.25rem] font-bold capitalize text-center"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
+          Latest Blog Posts
+        </motion.p>
+
+        <motion.p
+          className="text-[#828282] font-Inter text-[1.125rem font-normal text-center lg:w-[39.375em] self-center"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
+          Diam et habitasse tortor cras donec urna eget dolor in turpis
+          venenatis eget pulvinar ipsum quisque non arcu nulla
+        </motion.p>
+      </div>
+    </>
+  );
+}
--- src/blog/page.tsx
+++ src/blog/page.tsx
@@ -1,9 +1,11 @@
 import BlogHeroSection from "./_components/blog-hero-section";
+import BlogLatestPosts from "./_components/blog-latest-posts";
 
 export default function Blog() {
   return (
     <>
       <BlogHeroSection />
+      <BlogLatestPosts />
     </>
   );
 }
